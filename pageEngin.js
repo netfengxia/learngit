5db4aa084f3756b902b8de20acbb895598456fe8
netfengxia@163.com <netfengxia@163.com>
2015-07-10 17:04:51 +0800

    new

--- pageEngin.js
+++ pageEngin.js
@@ -1,3 +1,12 @@
 var TemplateEngine = funciton(tpl, data){
-	//
+	//...
+	var re = /<%([^%>]+)?%>/g;
+	while(match = re.exec(tpl)){
+		tpl = tpl.replace(match[0], data[match[1]]);
+	}
+
+	return tpl;
 }
+
+var template = '<p> Hello, my name is <%this.name%>. I\'m <%this.profileage%> years old.</p>';
+console.log(TemplateEngine(template,{name:'Krasimir', profile:{age:29}}));
